da7110d20f97f283e6b8e2b0e2a935e79bbbbf4f
Roneet Parida <roneetparida@gmail.com>
2017-11-11 12:20:47 +0530

    ui/ux improvements, performance fixs

--- functions/index.js
+++ functions/index.js
@@ -27,32 +27,39 @@ $(function() {
 
     width();
 
-
     //-----------------colors-------------------------
     $('ul.chips-list li').each(function() {
         //------------chips-color---------------
-        if($(this).hasClass('black')){
+        // if(i > 0){
             //---khali
-        }
-        else{
-            var m =  $(this).find('h1').html() + '500';
-            $(this).addClass(m);
-        }
-        // console.log(m);
+            var colorClass =  $(this).find('h1').html();
+            $(this).addClass(colorClass);
+        // }
+
+
+        // else{
+        //     var colorClass =  $(this).find('h1').html() + '500';
+        //     $(this).addClass(colorClass);
+        // }
+        // console.log(colorClass);
 
         //------------header-color----------
         $(this).on('click',function () {
-            if($(this).hasClass('black')){
-                $('#header').css('background', '#e0e0e0');
-            }
-            else{
-                var t = $(this).css('background-color');
-                $('#header').css('background', t);
-            }
-            // console.log(t);
+        //     if($(this).hasClass('black')){
+        //         $('#header').css('background', '#e0e0e0');
+        //     }
+        //     else{
+                var headerColor = $(this).css('background-color').replace(')', ', 0.2)').replace('rgb', 'rgba');
+                $('.search-wrapper').css('background', headerColor);
+                // $('#header').css('background-color').replace(')', ', 0.4)').replace('rgb', 'rgba');
+        //     }
+        //     console.log(headerColor);
         });
 
     });
+
+
+
 
 
     //---------color-hex-values-boxes
@@ -65,6 +72,7 @@ $(function() {
 
 
 
+
         // $(this).children().children('.copy-btn').on('click',function () {
         //
         //     // var copyText = $(this).children().children('.hex').html();
@@ -73,6 +81,13 @@ $(function() {
         //     // alert("Copied the text: " + hex.value);
         //     // console.log(hex);
         // });
+    });
+
+    $('.color-wrapper').each(function () {
+        $(this).append('<div class="select-btn"></div>');
+        var n = $(this).children('.colors:nth-child(5)').css('background-color');
+        $(this).children('.select-btn').css('background-color', n);
+        console.log(n);
     });
 
 
@@ -97,15 +112,15 @@ $(function() {
         $('body').css('overflow', 'visible');
     }
 
-    //nav-open----------------------------------------
-    $('svg.nav-btn').on('click',function () {
-        navOpen();
-    });
-
-    //nav-close----------------------------------------
-    $('.nav-overlay,li.menu-item').on('click',function () {
-        navClose();
-    });
+    // //nav-open----------------------------------------
+    // $('svg.nav-btn').on('click',function () {
+    //     navOpen();
+    // });
+    //
+    // //nav-close----------------------------------------
+    // $('.nav-overlay,li.menu-item').on('click',function () {
+    //     navClose();
+    // });
 
     //search-input-value-reset-------------------------------------------
     $('svg.search-close-btn').on('click',function () {
@@ -134,92 +149,59 @@ $(function() {
         }
     });
 
-    //nav-list-selection--------------------------------------------------
-    $('li.apps-item, li.bug-list').on('click',function () {
-        $('li.apps-item, li.bug-list').addClass('menu-item-selected');
-        $('li.bug-list, li.apps-item').not(this).removeClass('menu-item-selected');
-    });
+    // //nav-list-selection--------------------------------------------------
+    // $('li.apps-item, li.bug-list').on('click',function () {
+    //     $('li.apps-item, li.bug-list').addClass('menu-item-selected');
+    //     $('li.bug-list, li.apps-item').not(this).removeClass('menu-item-selected');
+    // });
 
     //chip-filter---------------------------------------
+
     var card = $('.card');
+    //-----other-selection--------------
+    function otherChipSelected() {
+        $('.chips').on('click',function () {
+            var h = $(this).attr('class').split(' ')[1];
+
+            card.removeClass('card-hidden');
+            //-----all-color-selected
+            $(this).addClass('color-selected');
+            $('.chips').not(this).removeClass('color-selected');
+            width();
+            //------selects-colors-from-chips
+            $('.colors').css('width','100%');
+            $('.colors .details').css('display','block')
+            //------hides-cards-which-are-not-selected
+            card.not('.' + h).addClass('card-hidden');
+            // console.log(h);
+            $('.select-btn').hide();
+
+        });
+
+    }
+    //all-card-selected------------
+    function allChipSelected() {
+        $('.chips.all').on('click',function () {
+            card.not('.all').removeClass('card-hidden');
+
+            $('.colors').css('width','50');
+            $('.colors .details').css('display','none')
+
+            $('.select-btn').show();
+
+        });
+
+    }
+
+    otherChipSelected();
+    allChipSelected();
 
 
-    $('ul.chips-list li').on('click',function () {
-        $(this).addClass('color-selected');
-        $('.chips').not(this).removeClass('color-selected');
-        width();
-    });
 
-    $('.chips').on('click',function () {
-        card.removeClass('card-hidden');
-        $('.colors').css('width','100%');
-        $('.colors .details').css('display','block')
-    });
-    $('.chips.black').on('click',function () {
-        $('.colors').css('width','50');
-        $('.colors .details').css('display','none')
-    });
 
-    $('.chips.red500').on('click',function () {
-        card.not('.red').addClass('card-hidden');
-    });
-    $('.chips.pink500').on('click',function () {
-        card.not('.pink').addClass('card-hidden');
-    });
-    $('.chips.purple500').on('click',function () {
-        card.not('.purle').addClass('card-hidden');
-    });
-    $('.chips.deep-purple500').on('click',function () {
-        card.not('.deep-purple').addClass('card-hidden');
-    });
-    $('.chips.indigo500').on('click',function () {
-        card.not('.indigo').addClass('card-hidden');
-    });
-    $('.chips.blue500').on('click',function () {
-        card.not('.blue').addClass('card-hidden');
-    });
-    $('.chips.light-blue500').on('click',function () {
-        card.not('.light-blue').addClass('card-hidden');
-    });
-    $('.chips.cyan500').on('click',function () {
-        card.not('.cyan').addClass('card-hidden');
-    });
-    $('.chips.teal500').on('click',function () {
-        card.not('.teal').addClass('card-hidden');
-    });
-    $('.chips.green500').on('click',function () {
-        card.not('.green').addClass('card-hidden');
-    });
-    $('.chips.light-green500').on('click',function () {
-        card.not('.light-green').addClass('card-hidden');
-    });
-    $('.chips.lime500').on('click',function () {
-        card.not('.lime').addClass('card-hidden');
-    });
-    $('.chips.yellow500').on('click',function () {
-        card.not('.yellow').addClass('card-hidden');
-    });
-    $('.chips.amber500').on('click',function () {
-        card.not('.amber').addClass('card-hidden');
-    });
-    $('.chips.orange500').on('click',function () {
-        card.not('.orange').addClass('card-hidden');
-    });
-    $('.chips.deep-orange500').on('click',function () {
-        card.not('.deep-orange').addClass('card-hidden');
-    });
 
-    $('.chips.brown500').on('click',function () {
-        card.not('.brown').addClass('card-hidden');
-    });
 
-    $('.chips.grey500').on('click',function () {
-        card.not('.grey').addClass('card-hidden');
-    });
 
-    $('.chips.blue-grey500').on('click',function () {
-        card.not('.blue-grey').addClass('card-hidden');
-    });
 
 
     // $(window).scroll(function() {
